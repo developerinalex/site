1b0783b9d95774568c1280e224e0b586f24033a3
allinux <alextrusting1@gmail.com>
2025-05-03 19:05:59 -0300

    nao to conseguindo fazer

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,5 @@
 function alerta() {
-    documnet.getElemntById("mensagem").innerHTML = "Obrigado por chegar até aqui! Nunca desita dos seu seonho e continue estudando!";
+    documnet.getElementById("mensagem").innerHTML = "Obrigado por chegar até aqui! Nunca desita dos seu seonho e continue estudando!";
 }
 
 document.getElementById("botao").addEventListener("click", alerta);
