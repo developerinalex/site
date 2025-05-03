75b73b4dbafc1dfb2c58dd8171038ccd571d4552
allinux <alextrusting1@gmail.com>
2025-05-03 18:56:16 -0300

    finalizando

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,5 @@
 function alerta() {
-    alert("Obrigado por clicar, até mais! E nunca desista dos seus sonhos!");
+    documnet.getElemntById("mensagem").innerHTML = "Obrigado por chegar até aqui! Nunca desita dos seu seonho e continue estudando!";
 }
 
 document.getElementById("botao").addEventListener("click", alerta);
